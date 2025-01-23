e5ee533f10fd2c1189e41ec4a04571bf613efcd7
kentaro iwai <iwai.kentaro@wiserock.jp>
2025-01-23 17:18:30 +0900

    プラグインのインストール

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,7 +1,6 @@
-// src/config/apiConfig.ts
 const apiConfig = {
     baseUrl: process.env.REACT_APP_API_URL || 'http://localhost:3000',
 };
-  
-  export default apiConfig;
+
+export default apiConfig;
   
